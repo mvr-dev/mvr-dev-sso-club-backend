eb5dc40e5ac614762ad5c41604c0724ecd00c34a
Vyacheslav Markov <mvr.here@vk.com>
2026-04-05 13:41:36 +0600

    removed suggestions to theirs own router

--- app/api/routers/signin.py
+++ app/api/routers/signin.py
@@ -4,8 +4,6 @@ from data.models.user import User
 from data.models import UserResponse, UserCreateRequest
 from services.iuser_service import IUserService
 import api.dependensies as dependensies
-from services.isuggestions_service import ISuggestionsService
-from typing import List, Optional
 import services.auth.utils as jwt_utils
 
 router = APIRouter()
@@ -22,7 +20,7 @@ async def register(
 async def auth_issue_jwt(user: User = Depends(dependensies.get_user_service().validate_user)):
     user = await user
     jwt_payload = {
-        'sub':user.id,
+        'sub': str(user.id),
         'name' : user.surname+" "+user.name,
         'email' : user.email
     }
@@ -31,20 +29,3 @@ async def auth_issue_jwt(user: User = Depends(dependensies.get_user_service().va
         access_token=token,
         token_type="bearer"
     )
-
-@router.get("/names",response_model=List[str])
-async def getNames(prefix:str ="", suggestionsService: ISuggestionsService = Depends(dependensies.get_names_suggestions_service)):
-    # print(suggestionsService.getNames())
-    return await suggestionsService.getSuggestion(prefix)
-
-@router.get("/surnames",response_model=List[str])
-async def getNames(prefix:str ="", suggestionsService: ISuggestionsService = Depends(dependensies.get_surnames_suggestion_service)):
-    return await suggestionsService.getSuggestion(prefix)
-
-@router.get('/cities',response_model=List[str])
-async def getCities(prefix:str = "", suggestionsService: ISuggestionsService = Depends(dependensies.get_cities_suggestion_service)):
-    return await suggestionsService.getSuggestion(prefix)
-
-@router.get('/streets',response_model=List[str])
-async def getStreets(prefix:str = "", suggestionService:ISuggestionsService = Depends(dependensies.get_streets_suggestion_service)):
-    return await suggestionService.getSuggestion(prefix)
--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -3,12 +3,24 @@ from data.models import UserResponse, UserCreateRequest
 from services.iuser_service import IUserService
 import api.dependensies as dependensies
 from services.isuggestions_service import ISuggestionsService
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import List, Optional
+from services.auth.utils import decode
 
 router = APIRouter()
+http_bearer = HTTPBearer()
+
+async def get_current_auth_user(
+        token : HTTPAuthorizationCredentials = Depends(http_bearer),
+        userService: IUserService = Depends(dependensies.get_user_service)
+    ):
+    payload = decode(token=token.credentials)
+    print(payload)
+    return await userService.getUserById(int(payload["sub"]))
+    
 
 @router.get("/user",response_model=UserResponse)
-async def getNames(id: int, userService: IUserService = Depends(dependensies.get_user_service)):
+async def getNames(user = Depends(get_current_auth_user)):
     # print(suggestionsService.getNames())
-    return await userService.getUserById(id)
+    return  user
 
